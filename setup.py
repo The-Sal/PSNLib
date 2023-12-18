8ffacb135c85fa624b3dbd98ed8eac792e82c966
Sal <salmanfaris2005@hotmail.com>
2023-12-18 08:52:53 +0400

    Added case if psn remote play is already in foreground

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
     name='PSNLib',
-    version='0.0.2–dev',
+    version='0.0.2',
     packages=find_packages(),
     package_data={
         'PSNLib': ['assets/PSNOCR', 'assets/*.png'],
